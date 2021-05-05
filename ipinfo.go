9346060ff9a33361a2aab454f4f7a17f80bd1e18
Prashant Atman <oracle.prashantprakash@gmail.com>
2021-05-05 14:02:19 +0200

    Update ipinfo.go

--- ipinfo.go
+++ ipinfo.go
@@ -1,13 +1,26 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 )
 
+type IPINFO struct {
+	IP           string `json:"ip"`
+	Hostname     string `json:"hostname"`
+	City         string `json:"city"`
+	Region       string `json:"region"`
+	Country      string `json:"country"`
+	Location     string `json:"loc"`
+	Organisation string `json:"org"`
+	Postal       string `json:"postal"`
+	Timezone     string `json:"timezone"`
+}
+
 func main() {
-	url := "https://ipinfo.io/?token=<YOUR-TOKEN>"
+	url := "https://ipinfo.io/?token=YOUR_TOKEN"
 	method := "GET"
 	client := &http.Client{}
 
@@ -30,5 +43,20 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	fmt.Println(string(body))
+	// fmt.Println(string(body))
+	var ipinfo IPINFO
+	err = json.Unmarshal(body, &ipinfo)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("IP           : " + ipinfo.IP)
+	fmt.Println("Hostname     : " + ipinfo.Hostname)
+	fmt.Println("City         : " + ipinfo.City)
+	fmt.Println("Region       : " + ipinfo.Region)
+	fmt.Println("Country      : " + ipinfo.Country)
+	fmt.Println("Location     : " + ipinfo.Location)
+	fmt.Println("Organisation : " + ipinfo.Organisation)
+	fmt.Println("Postal       : " + ipinfo.Postal)
+	fmt.Println("Timezone     : " + ipinfo.Timezone)
 }
